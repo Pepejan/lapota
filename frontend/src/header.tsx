38a1d15f610e2a509674996447c05dea44aa1d3d
Pepejan <sasabondarcuk06@gmail.com>
2025-01-10 21:45:55 +0200

    feat: add btn log/reg, and some style header

--- frontend/src/header.tsx
+++ frontend/src/header.tsx
@@ -1,20 +1,35 @@
 function Header() {
-  return (
-    <nav className="navbar navbar-expand-lg bg-secondary">
-      <div className="container-fluid">
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-          </ul>
-          <span className="navbar-text">Link shortener</span>
+    return (
+        <div className="col-m container bg-primary">
+            <div className="row justify-content-md-center">
+                <nav className="navbar navbar-expand-sm">
+                    <div className="container-fluid">
+
+                        <div className="collapse navbar-collapse" id="navbarText">
+                            <div className="navbar-nav col col-lg-1">
+                            </div>
+
+                            <div className="navbar-nav  me-auto mb-2 mb-lg-0 col-lg-2 nav-item">
+                                <a className=" custom-css nav-link active" href="#">Home</a>
+                            </div>
+
+                            <div className="col col-lg-2">
+                                <a type="button"
+                                   className="custom-css btn btn-primary  btn-outline-light btn-lg m-1"
+                                   id="LoginBtn">Login</a>
+                                <a type="button"
+                                   className="custom-css btn btn-primary bg-success btn-outline-light btn-lg"
+                                   id="RegisterBtn">Sign up</a>
+                            </div>
+
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </div>
-      </div>
-    </nav>
-  );
+    )
 }
 
-export default Header;
+
+export default Header
+
